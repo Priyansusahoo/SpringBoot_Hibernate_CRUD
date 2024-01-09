4da79f24e9bedcb7e8aa6a7d910f72551ceb96d9
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-01-09 17:53:13 +0530

    Update CrudRepo.java

--- src/main/java/com/example/demo/repo/CrudRepo.java
+++ src/main/java/com/example/demo/repo/CrudRepo.java
@@ -1,20 +1,17 @@
 package com.example.demo.repo;
 
 import com.example.demo.entity.EntityClass;
-import lombok.experimental.PackagePrivate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import javax.transaction.Transactional;
-import java.beans.Transient;
-import java.util.List;
 import java.util.Optional;
 
 public interface CrudRepo extends JpaRepository<EntityClass, Long> {
     Optional<EntityClass> findById(Long id);
     @Modifying
+    @Transactional
     void deleteById(Long id);
 
     @Modifying
